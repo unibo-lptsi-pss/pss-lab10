cb2a121c175c453452f1ad43823b4a82c38bdaff
DanySK <danilo.pianini@gmail.com>
2022-11-24 10:56:04 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@79e055ce6ce6c851870d338030ef4079beeb82f7 🚀

--- 02-read-write-files/src/main/java/it/unibo/io/LineNumbers.java
+++ 02-read-write-files/src/main/java/it/unibo/io/LineNumbers.java
@@ -1,6 +1,10 @@
 package it.unibo.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
 /**
  * Simple utility class that adds line numbers to files.
@@ -22,5 +26,14 @@ public final class LineNumbers {
      * @param file the file on which the function should operate
      */
     public static void addLineNumbersToTextFile(final File file) throws IOException {
+        final var lines = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
+        final var numberedLines = new ArrayList<String>(lines.size());
+        for (int lineNumber = 1; lineNumber <= lines.size(); lineNumber++) {
+            numberedLines.add(lineNumber + " " + lines.get(lineNumber - 1));
+        }
+        final var destination = new File(
+            file.getParentFile().getAbsolutePath() + File.separator + "numbered-" + file.getName()
+        );
+        Files.write(destination.toPath(), numberedLines, StandardCharsets.UTF_8);
     }
 }

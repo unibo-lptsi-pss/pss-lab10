c02ca8a7dbfa4013032fdf036fb64744fdc9434e
DanySK <danilo.pianini@gmail.com>
2022-11-24 16:27:17 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@058dfae9acf05997c5b2de758e5a71788bc59ef9 🚀

---
+++ 03-read-classpath-write-file/src/main/java/it/unibo/io/TarantulaNebula.java
@@ -0,0 +1,25 @@
+package it.unibo.io;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.ClassLoader.getSystemResource;
+
+/**
+ * Application entry point.
+ */
+public final class TarantulaNebula {
+
+    private static final String RESOURCE_PATH = "it/unibo/io/";
+
+    private TarantulaNebula() { }
+
+    /**
+     * Application entry point. Creates documentation about the Tarantula Nebula in the target folder.
+     * @param args one mandatory argument representing the destination path
+     */
+    public static void main(final String[] args) {
+        // Implement here. Add any other classes as desired, if needed.
+    }
+}

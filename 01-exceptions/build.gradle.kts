9b6c0d52a71f1863d88e7437b7bf909390829879
pianinator <113355583+pianinator@users.noreply.github.com>
2022-11-24 09:18:15 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@5da68e3c149f783ccf3268324c7d425501a27027 🚀

--- 01-exceptions/build.gradle.kts
+++ 01-exceptions/build.gradle.kts
@@ -1,3 +1,15 @@
+import org.gradle.kotlin.dsl.registering
+
 plugins {
+    application
     java
+    id("org.danilopianini.gradle-java-qa") version "0.40.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("it.unibo.exceptions.UseArithmeticService")
 }

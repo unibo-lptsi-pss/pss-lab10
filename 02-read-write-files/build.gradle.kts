4e8679f8859507c4570a789a22fb8fe3fe3fc38a
pianinator <113355583+pianinator@users.noreply.github.com>
2024-08-01 14:50:24 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@998447902213d76e43cb62df6a66a04cdae47daa 🚀

--- 02-read-write-files/build.gradle.kts
+++ 02-read-write-files/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     application
     java
-    id("org.danilopianini.gradle-java-qa") version "1.57.1"
+    id("org.danilopianini.gradle-java-qa") version "1.60.2"
 }
 
 repositories {

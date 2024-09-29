70518c84e9ca99aaf47274367fdb05d1f26f7a0f
pianinator <113355583+pianinator@users.noreply.github.com>
2024-09-29 08:44:14 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@bd7bdccfa3842f68b392231745cf3246bdb0b751 🚀

--- 02-read-write-files/build.gradle.kts
+++ 02-read-write-files/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    val junitVersion = "5.11.0"
+    val junitVersion = "5.11.1"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")

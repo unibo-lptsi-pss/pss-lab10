8bc92444865f13842f9aed9dff2eae28dce3de8a
DanySK <danilo.pianini@gmail.com>
2025-02-17 17:41:28 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@c46df0e96866723a53ec5d7aee2fd8ae27c4f358 🚀

--- 02-maps-advanced/build.gradle.kts
+++ 02-maps-advanced/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.kotlin.dsl.registering
 plugins {
     application
     java
-    id("org.danilopianini.gradle-java-qa") version "1.91.0"
+    id("org.danilopianini.gradle-java-qa") version "1.96.0"
 }
 
 repositories {
